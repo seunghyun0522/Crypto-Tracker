2305e01eefea98a4704681556b827dac00058184
seunghyun <cvtcvt007@naver.com>
2023-05-11 20:43:25 +0900

    #5.9 React Query part One

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,29 +1,34 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
+
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
 const CoinsList = styled.ul``;
+
 const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.bgColor};
-  margin-bottom: 10px;
-
   border-radius: 15px;
+  margin-bottom: 10px;
   a {
-    padding: 20px;
-    transition: color 0.2s ease-in;
     display: flex;
     align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
   }
   &:hover {
     a {
@@ -36,10 +41,10 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
+
 const Loader = styled.span`
   text-align: center;
   display: block;
-  margin-top: 30px;
 `;
 
 const Img = styled.img`
@@ -47,7 +52,8 @@ const Img = styled.img`
   height: 35px;
   margin-right: 10px;
 `;
-interface CoinInterface {
+
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -58,31 +64,23 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
-
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {coins.map((coin) => (
+          {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link
-                to={{ pathname: `/${coin.id}`, state: { name: coin.name } }}
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
               >
                 <Img
                   src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}

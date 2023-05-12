f5e4104b68eea146348cf61a4f0b5b2368dffbbe
seunghyun0522 <cvtcvt007@naver.com>
2023-05-12 12:05:25 +0900

    change USD

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -119,27 +119,7 @@ interface PriceData {
   beta_value: number;
   first_data_at: string;
   last_updated: string;
-  quotes: {
-    USD: {
-      ath_date: string;
-      ath_price: number;
-      market_cap: number;
-      market_cap_change_24h: number;
-      percent_change_1h: number;
-      percent_change_1y: number;
-      percent_change_6h: number;
-      percent_change_7d: number;
-      percent_change_12h: number;
-      percent_change_15m: number;
-      percent_change_24h: number;
-      percent_change_30d: number;
-      percent_change_30m: number;
-      percent_from_price_ath: number;
-      price: number;
-      volume_24h: number;
-      volume_24h_change_24h: number;
-    };
-  };
+  quotes: {};
 }
 
 function Coin() {
@@ -163,7 +143,6 @@ function Coin() {
     <Container>
       <Helmet>
         <title>
-          {" "}
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
         </title>
       </Helmet>
@@ -187,7 +166,6 @@ function Coin() {
             </OverviewItem>
             <OverviewItem>
               <span>Price :</span>
-              <span>{tickersData?.quotes.USD.price}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>

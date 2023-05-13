57c14c4e0c8ed65e1c670baf4a2bd20cf6387b8c
seunghyun0522 <cvtcvt007@naver.com>
2023-05-13 22:48:53 +0900

    1

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { fetchCoins } from "../api";
 import { Helmet } from "react-helmet";
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 10px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
